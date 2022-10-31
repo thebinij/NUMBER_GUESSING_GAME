0153a97b1fc0b089dfe6ab5aaa4c802e6c6a2bca
thebinij <binijshrestha@gmail.com>
2022-10-31 06:09:51 +0000

    fix: update best guess properly

--- number_guess.sh
+++ number_guess.sh
@@ -2,23 +2,23 @@
 PSQL="psql --username=freecodecamp --dbname=number_guess --tuples-only --no-align -c"
 # Initializing Random number between 1 to 1000
 SECRET="$(echo $(($RANDOM % 1000 + 1)))"
-
 echo "Enter your username: "
 read USERNAME
-USER_DETAIL=$($PSQL "SELECT * FROM users WHERE name = '$USERNAME'")
+USER_DETAIL=$($PSQL "SELECT * FROM users WHERE username = '$USERNAME'")
 
 # if user not found
 if [[ -z $USER_DETAIL ]]
 then
   echo "Welcome, $USERNAME! It looks like this is your first time here."
-  INSERT_USER=$($PSQL "INSERT INTO users(name) VALUES('$USERNAME') ")  
+  INSERT_USER=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME') ")  
 else
   echo $USER_DETAIL | while IFS='|' read NAME GAME_PLAYED BEST_NO_GUESS
   do
-  echo "Welcome back, $NAME! You have played $GAME_PLAYED games, and your best game took $BEST_NO_GUESS guesses."
+    echo "Welcome back, $NAME! You have played $GAME_PLAYED games, and your best game took $BEST_NO_GUESS guesses."
   done
 fi
 NO_OF_GUESSES=0
+
 
 START_GAME(){
     if [[ -z $1 ]]
@@ -48,12 +48,17 @@ START_GAME(){
       else
         echo "You guessed it in $NO_OF_GUESSES tries. The secret number was $SECRET. Nice job!"
         # update games played
-        UPDATE_GAME_PLAYED=$($PSQL "UPDATE users SET games_played = games_played +1 WHERE name = '$USERNAME'") 
-        # update best_game if lower than previous
-        if [[ $NO_OF_GUESSES -lt $BEST_NO_GUESS ]]
-        then
-          UPDATE_BEST_GAME=$($PSQL "UPDATE users SET guesses_in_best_game = $NO_OF_GUESSES WHERE name = '$USERNAME'") 
-        fi
+        UPDATE_GAME_PLAYED=$($PSQL "UPDATE users SET games_played = games_played +1 WHERE username = '$USERNAME'") 
+        
+        echo $USER_DETAIL| while IFS='|' read NAME GAME_PLAYED BEST_NO_GUESS
+        do
+          # if best_no_guess not found or lower than previous best
+          if [[ -z $BEST_NO_GUESS || $NO_OF_GUESSES -lt $BEST_NO_GUESS  ]]
+          then
+             UPDATE_BEST_GAME=$($PSQL "UPDATE users SET best_game = $NO_OF_GUESSES WHERE username = '$USERNAME'") 
+          fi
+        done
+        
       fi
     fi
 }
